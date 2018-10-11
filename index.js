f154ed931f2b441cb0ee26eb64703e3baf1cd585
Katrina Pritchard <pritckat@gmail.com>
2018-10-11 00:14:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(location) {
-  return location - 42
+  if (location >= 42) {
+    return location - 42;
+  }
+  else if (location < 42) {
+    return 42 - location;
+  }
 }
