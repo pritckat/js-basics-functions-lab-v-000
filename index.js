5906693a0a8088a3ec5ff99bbf9c15eccc3d52df
Katrina Pritchard <pritckat@gmail.com>
2018-10-11 00:19:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,6 @@ function distanceFromHqInFeet(blocks) {
 }
 
 function distanceTravelledInFeet(start, stop) {
-  return (stop - start) * 264
+  distance = (stop - start) * 264
+  distance > 0 ? distance : distance * -1;
 }
